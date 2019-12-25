59b6813104737832e32ddfbbcf1d7a4c5d04e455
winby <winbywenbo@gmail.com>
2019-12-25 23:27:00 +0800

    sort

--- src/main/java/dataStructure/graph/Graph.java
+++ src/main/java/dataStructure/graph/Graph.java
@@ -1,51 +1,51 @@
 package main.java.dataStructure.graph;
 
+
+
 /*
  * 定义图的结构
  */
 public class Graph {
-	//节点数目
-	protected int size=20;
-	//定义数组，保存顶点信息
-	protected String[] nodes;
+    //节点数目
+    protected int size;
+    //定义数组，保存顶点信息
+    protected String[] nodes;
 
-	//定义矩阵保存顶点信息
-	protected int[][] edges;
+    //定义矩阵保存顶点信息
+    protected int[][] edges;
 
-	protected int[] visit;            //遍历标志
+    /**
+     *      A B C D E F G
+     *   A  0 0 1 1 0 1 0
+     *   B  0 0 1 0 0 0 0
+     *   C  1 1 0 1 0 0 0
+     *   D  1 0 1 0 0 0 0
+     *   E  0 0 0 0 0 0 1
+     *   F  1 0 0 0 0 0 1
+     *   G  0 0 0 0 1 1 0
+     */
+    public Graph(){
+        //初始化顶点
+        nodes = new String[]{"A","B","C","D","E","F","G"};
+        size=nodes.length;
 
-	/**
-	 *      vo v1 v2 v3
-	 *   vo  0  1  1  1
-	 *   v1  1  0  1  0
-	 *   v2  1  1  0  1
-	 *   v3  1  0  1  0
-	 */
-	public Graph(){
-		size=4;
+        //初始化边---- 为了直观，做一个常量定义
+        final int A=0,B=1,C=2,D=3,E=4,F=5,G=6;
+        edges = new int[size][size];
+        edges[A][C] = 1;
+        edges[A][D] = 1;
+        edges[A][F] = 1;
+        edges[B][C] = 1;
+        edges[C][A] = 1;
+        edges[C][D] = 1;
+        edges[C][B] = 1;
+        edges[D][A] = 1;
+        edges[D][C] = 1;
+        edges[E][G] = 1;
+        edges[F][A] = 1;
+        edges[F][G] = 1;
+        edges[G][F] = 1;
+        edges[G][E] = 1;
+    }
 
-		//初始化顶点
-		nodes = new String[size];
-		for (int i=0;i<size;i++){
-			nodes[i] = String.valueOf(i);
-		}
-
-		//初始化边
-		edges = new int[size][size];
-		edges[0][1] = 1;
-		edges[0][2] = 1;
-		edges[0][3] = 1;
-		edges[1][0] = 1;
-		edges[1][2] = 1;
-		edges[2][0] = 1;
-		edges[2][1] = 1;
-		edges[2][3] = 1;
-		edges[3][0] = 1;
-		edges[3][2] = 1;
-	}
-
-	public static void main(String[] args) {
-		Graph graph = new Graph();
-
-	}
 }

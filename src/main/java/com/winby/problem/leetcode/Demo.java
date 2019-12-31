66abc67fd2123ee5e1920bc281815b797bbc1946
winby <winbywenbo@gmail.com>
2020-01-01 00:10:56 +0800

    13. 罗马数字转整数  romanToInt

---
+++ src/main/java/com/winby/problem/leetcode/Demo.java
@@ -0,0 +1,28 @@
+package com.winby.problem.leetcode;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+
+
+
+ */
+public class Demo {
+    /**
+     * 顺序遍历
+     */
+    public static Object solution1(Object x) {
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        Object result;
+
+        Object x = 1234;
+        result = solution1(x);
+        System.out.println(JSON.toJSON(result));
+    }
+
+}
